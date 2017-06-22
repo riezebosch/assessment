561cfad2acb7f28d87e6f2b3c662f5ae50ba1b17
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-22 21:41:10 +0200

    test json stringify entry

--- api.tests/Controllers/ValuesControllerTests.cs
+++ api.tests/Controllers/ValuesControllerTests.cs
@@ -1,4 +1,6 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Xunit;
 
 namespace api.Controllers.tests
@@ -10,6 +12,23 @@ namespace api.Controllers.tests
         {
             var controller = new ValuesController();
             Assert.Equal(new []{"value1", "value2"}, controller.Get());
+        }
+
+        [Fact]
+        public void JsonTest()
+        {
+            var entry = new Entry { Word = "aap", Translation = "monkey" };
+            var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+            var json = JsonConvert.SerializeObject(entry, settings);
+            
+            Assert.Equal(@"{""word"":""aap"",""translation"":""monkey""}", json);
+        }
+
+        private class Entry
+        {
+            public string Word { get; set; }
+            public string Translation { get; set; }
         }
     }
 }
