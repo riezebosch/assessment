3b69f854e692a3acb0bcd17c445b2733d2ac14e0
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-22 22:12:28 +0200

    sln and api.db/api.db.tests with sqlite

---
+++ api.db.tests/EntryTests.cs
@@ -0,0 +1,20 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Xunit;
+
+namespace api.db.tests 
+{
+    public class EntryTests 
+    {
+         [Fact]
+        public void JsonTest()
+        {
+            var entry = new Entry { Word = "aap", Translation = "monkey" };
+            var settings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+            var json = JsonConvert.SerializeObject(entry, settings);
+            
+            Assert.Equal(@"{""id"":0,""word"":""aap"",""translation"":""monkey""}", json);
+        }
+    }
+}
---
+++ api.db/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace api.db
+{
+    public class Entry
+    {
+        public int Id { get; set; }
+        public string Word { get; set; }
+        public string Translation { get; set; }
+    }
+}
