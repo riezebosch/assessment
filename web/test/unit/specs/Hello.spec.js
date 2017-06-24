f6307bf4980e8f89f944e1dcc080ae26bd88607d
Manuel Riezebosch <mriezebosch@gmail.com>
2017-06-24 17:33:16 +0200

    unit test werkend met zetten van data

--- web/test/unit/specs/Hello.spec.js
+++ web/test/unit/specs/Hello.spec.js
@@ -9,10 +9,18 @@ describe('Hello.vue', () => {
       .to.equal('Dierennamen')
   })
 
-  it('should render the entries', () => {
+  it('should render the entries', (done) => {
     const Constructor = Vue.extend(Hello)
     const vm = new Constructor().$mount()
-    expect(vm.$el.querySelector('.hello ul li span:first-child').textContent)
-      .to.equal('aap')
+    vm.$data.entries = [{
+      id: 1, word: 'aap', 'translation': 'monkey'
+    }]
+
+    vm.$nextTick(() => {
+      expect(vm.$el.querySelector('.hello ul li span:first-child').textContent)
+        .to.equal('aap')
+
+      done()
+    })
   })
 })
